5da41c299a6c787bcb07d276eb2db8c75f4809cc
肖玉萃 <xiaoyc@didikon.com>
2016-03-27 14:59:49 +0800

    sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160327014650) do
+ActiveRecord::Schema.define(version: 20160327020527) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
--- environments.rb
+++ environments.rb
@@ -1,9 +1,14 @@
+# require 'mongo'
+
+
 
 configure :development do
- ActiveRecord::Base.establish_connection(
-  :adapter => 'sqlite3',
-  :database =>  'sinatra_application.sqlite3.db'
-)
+    ActiveRecord::Base.establish_connection(
+        :adapter => 'sqlite3',
+        :database =>  'sinatra_application.sqlite3.db'
+    )
+    # db = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'development_blog')  
+    # set :mongo_db, db[:development_blog]
 end
 
 # configure :production do
