ee0303bacf3dd891e4d55beab23b2aff08b9cec3
肖玉萃 <xiaoyc@didikon.com>
2016-04-01 21:28:40 +0800

    tp

--- apis/api.rb
+++ apis/api.rb
@@ -6,6 +6,7 @@ Dir.glob("#{__dir__}/*.rb").each { |r| require_relative r }
 class API < Grape::API
   prefix :api # set the url prefix
   format :json
+  content_type :json, 'application/json;charset=UTF-8'
 
   mount PostsAPI
 end
