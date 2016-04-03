fd19416fc07fc192e116681f81ce17a2ab387004
肖玉萃 <xiaoyc@didikon.com>
2016-04-03 22:01:16 +0800

    post create

--- apis/posts_api.rb
+++ apis/posts_api.rb
@@ -2,9 +2,23 @@ class PostsAPI < Grape::API
   resource :posts do
     desc '获取帖子列表'
     get do
-      present ::Post.all, with: Entities::Post
+      present ::Post.all.order(created_at: :desc), with: Entities::Post
     end
 
+    desc '创建帖子'
+    params do
+        requires :title, type: String
+        requires :body, type: String
+    end
+    post do
+        status 200
+        if Post.create(title: params[:title], body: params[:body])
+            {success: true}
+        else
+            {success: false}
+        end
+
+    end
     
     params do
         requires :id
