5934612d56cef672f2dd0573cecd14dd9bd2eb0e
肖玉萃 <xiaoyc@didikon.com>
2016-04-03 20:06:23 +0800

    show

--- apis/entities/post.rb
+++ apis/entities/post.rb
@@ -1,8 +1,11 @@
 module Entities
     class Post < Grape::Entity
         root 'posts'
+        expose :id, documentation: {type: Integer, desc: '帖子id'}
         expose :title, documentation: {type: String, desc: '帖子标题'}
         expose :body, documentation: {type: String, desc: '帖子内容'}
-        expose :created_at
+        expose :created_at do |post|
+            post.created_at.strftime('%Y-%m-%d %H:%M:%S')
+        end
     end
 end
--- apis/posts_api.rb
+++ apis/posts_api.rb
@@ -4,5 +4,16 @@ class PostsAPI < Grape::API
     get do
       present ::Post.all, with: Entities::Post
     end
+
+    
+    params do
+        requires :id
+    end
+    route_param :id do
+        desc '获取帖子详情' 
+        get do
+            present Post.find(params[:id]), with: Entities::Post
+        end
+    end
   end
 end
